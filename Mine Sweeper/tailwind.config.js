df98c99dfcf46adb609fb93f583d3abfda983d62
jenkinsakp <unuavworhoakpevwe@gmail.com>
2024-05-21 00:20:30 +0400

    Add configuration to tailwind

--- Mine Sweeper/tailwind.config.js
+++ Mine Sweeper/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './public/index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
-  darkMode:false, // or 'media' or 'class'
+  darkMode:'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
